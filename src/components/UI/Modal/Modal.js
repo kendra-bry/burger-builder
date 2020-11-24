cdf4f4fbe5c7a8fd981044d779e2a26b34b89572
Kendra Bryant <kendraa@utahcounty.gov>
2020-11-23 22:15:17 -0700

    Modal rerenders when shown

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Modal.css";
 import Aux from '../../../hoc/Auxi';
 import Backdrop from '../Backdrop/Backdrop'
 
-const Modal = ({ children, show, modalClosed }) => {
-  return (
-    <Aux>
-      <Backdrop show={show} clicked={modalClosed} />
-      <div
-        className="modal"
-        style={{
-          transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: show ? '1' : '0'
-        }}>
-        {children}
-      </div>
-    </Aux>
-  )
-}
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  render() {
+    const { children, show, modalClosed } = this.props;
+    return (
+      <Aux>
+        <Backdrop show={show} clicked={modalClosed} />
+        <div
+          className="modal"
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: show ? '1' : '0'
+          }}>
+          {children}
+        </div>
+      </Aux>
+    )
+  }
+};
 
 export default Modal;
