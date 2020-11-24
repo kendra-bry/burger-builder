c88787057d2e1c12658b3ff84a0b3fe559c397bf
Kendra Bryant <kendraa@utahcounty.gov>
2020-11-23 21:58:13 -0700

    Converted Layout to container

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxi';
+import './Layout.css'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSideDrawer: false,
+    };
+  }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+  sideDrawerToggleHandler = () => {
+    this.setState((state) => ({ showSideDrawer: !state.showSideDrawer }));
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar toggled={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} />
+        <main className='content'>{this.props.children}</main>
+      </Aux >
+    );
+
+  };
+};
+
+export default Layout;
