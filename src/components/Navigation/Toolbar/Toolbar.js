c39523afa6fd116ea3fd46f95daa3d34e0329528
Kendra Bryant <kendraa@utahcounty.gov>
2020-11-23 21:58:59 -0700

    Created Toolbar component

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import Items from '../Items/Items';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
+
+const Toolbar = ({ open, toggled }) => {
+  return (
+    <header className="toolbar">
+      <div className="toolbarLogo">
+        <Logo />
+      </div>
+      <DrawerToggle clicked={toggled} />
+      <nav className="desktop">
+        <Items />
+      </nav>
+    </header>
+  );
+}
+
+export default Toolbar;
