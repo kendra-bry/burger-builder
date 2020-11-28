94a7ff62a621d1cc412e9709927eb0c05f03d632
Kendra Bryant <kendraa@utahcounty.gov>
2020-11-28 15:51:41 -0700

    Added routing to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import { Route, Switch } from 'react-router-dom';
 import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 class App extends Component {
-	render() {
-		return (
-			<div>
-				<Layout>
-					<BurgerBuilder /> 
-				</Layout>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
