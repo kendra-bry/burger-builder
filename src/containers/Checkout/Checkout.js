d3465628bab99f9918ef3c68bc867263193d2542
Kendra Bryant <kendraa@utahcounty.gov>
2020-11-28 15:51:16 -0700

    Created Checkout container

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
+
+class Checkout extends Component {
+  state = {
+    ingredients: null,
+    price: 0,
+  }
+
+  componentWillMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = JSON.parse(decodeURIComponent(query.get('ingredients')));
+    const price = decodeURIComponent(query.get('price'));
+    this.setState({ ingredients, price });
+  }
+
+  checkoutCanceledHandler = () => {
+    this.props.history.goBack();
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
+
+  render() {
+    const { path } = this.props.match;
+    return (
+      <div>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCanceled={this.checkoutCanceledHandler}
+          checkoutContinued={this.checkoutContinuedHandler} />
+        <Route
+          path={`${path}/contact-data`}
+          render={(props) => <ContactData
+            {...props}
+            ingredients={this.state.ingredients}
+            price={this.state.price} />} />
+      </div>
+    );
+  }
+}
+
+export default Checkout;
